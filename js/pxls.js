7bf9a2293d5f6677dd6550c56281f63bf23d78e5
Prakhar Gautam <prakhargautam96@gmail.com>
2015-10-15 17:33:42 +0530

    Update pxls.js

--- js/pxls.js
+++ js/pxls.js
@@ -3,28 +3,68 @@ var gridVal=0;
 $(document).ready(function(){
     
     $('input[name=clear]').click(function(){
-        $('.pixel').css('background-color','#E2E2E2');
+        $('.pixel').css('background-color','#FFF');
+        draw();
+        
     });
     $('input[name=grid]').click(function(){
         gridVal=prompt("Enter size of grid");
         clear();
         create(gridVal);
     });
-    $('input[name=erase]').click(function(){
+    $('input[name=eraser]').click(function(){
         erase();
     });
     $('input[name=pen]').click(function(){
         draw();
     });
-    $('.pixel').click(function(){
-        draw();
+    $('input[name=random]').click(function(){
+        random_draw();
     });
 });
+
+function random_draw()
+{
+    var f=0;
+    $('.pixel').click(function(){
+        var r=Math.floor(255*Math.random());
+        var g=Math.floor(255*Math.random());
+        var b=Math.floor(255*Math.random());
+        $(this).css('background-color','rgb('+r+','+g+','+b+')');
+            if(f===0)
+                f=1;
+            else
+                f=0;
+    });
+    $('.pixel').hover(function(){
+        if(f===1){
+        var r=Math.floor(255*Math.random());
+        var g=Math.floor(255*Math.random());
+        var b=Math.floor(255*Math.random());
+        $(this).css('background-color','rgb('+r+','+g+','+b+')');//rgb(r,g,b)
+        }
+    });
+}
+
+function increment()
+{
     
+}
+
 function erase()
 {
+    var f=0;
+    $('.pixel').click(function(){
+        $(this).css('background-color','#FFF');
+        if(f===0)
+            f=1;
+        else
+            f=0;
+    });
     $('.pixel').hover(function(){
-    $(this).css('background-color','#E2E2E2');
+        if(f===1){
+        $(this).css('background-color','#FFF');
+        }
     });
 }
 
@@ -58,15 +98,30 @@ function create(size)
     $('body').css('line-width',ppi+'px');
     $('.pixel').css('height',ppi+'px');
     $('.pixel').css('width',ppi+'px');
-
+    
 }
 
 function draw()
 {
-    /*$('.pixel').mouseover(function(){
+    var f=0;
+    $('.pixel').click(function(){
         $(this).css('background-color','#000');
-    });*/  
+            if(f===0)
+                f=1;
+            else
+                f=0;
+    });
+    
+    /*$('.pixel').click(function(){
+        return;
+    });*/
     $('.pixel').hover(function(){
-            $(this).css('background-color','#000');
-        });
+        if(f===1){
+        $(this).css('background-color','#000');
+        }
+    });
+}
+function draw2()
+{
+    
 }
